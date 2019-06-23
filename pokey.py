809dde55c452676c7754e90445ee8bc60146fa41
James Tomasino <james@tomasino.org>
2019-06-23 18:47:54 -0400

    multithreading socket support

--- pokey.py
+++ pokey.py
@@ -4,32 +4,66 @@ import logging
 import sys
 import time
 import socket
+import select
+import queue as Queue
 
-HOST = ''   # Symbolic name, meaning all available interfaces
 PORT = 8775 # Arbitrary non-privileged port
 
 def main():
+    # Init logger
     root_logger = logging.getLogger()
     root_logger.setLevel("INFO")
     root_logger.addHandler(SystemdHandler())
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    logging.info('Socket created')
+
+    # create async socket
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.setblocking(0)
 
     try:
-        s.bind((HOST, PORT))
-        logging.info('Socket bind complete')
-        s.listen(10)
-        logging.info('Socket now listening on port ' + str(PORT))
-        while 1:
-            conn, addr = s.accept()
-            logging.info('Connected with ' + addr[0] + ':' + str(addr[1]))
+        server.bind(('localhost', PORT))
+        server.listen(5)
+        inputs = [server]
+        outputs = []
+        message_queues = {}
     except socket.error as msg:
         logging.error('Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
-    except KeyboardInterrupt:
-        logging.info('Interrupt received, stopping…')
-    finally:
-        s.close()
-        sys.exit()
+
+    while inputs:
+        readable, writable, exceptional = select.select(
+            inputs, outputs, inputs)
+        for s in readable:
+            if s is server:
+                connection, client_address = s.accept()
+                connection.setblocking(0)
+                inputs.append(connection)
+                message_queues[connection] = Queue.Queue()
+            else:
+                data = s.recv(1024)
+                if data:
+                    message_queues[s].put(data)
+                    if s not in outputs:
+                        outputs.append(s)
+                else:
+                    if s in outputs:
+                        outputs.remove(s)
+                    inputs.remove(s)
+                    s.close()
+                    del message_queues[s]
+
+        for s in writable:
+            try:
+                next_msg = message_queues[s].get_nowait()
+            except Queue.Empty:
+                outputs.remove(s)
+            else:
+                s.send(next_msg)
+
+        for s in exceptional:
+            inputs.remove(s)
+            if s in outputs:
+                outputs.remove(s)
+            s.close()
+            del message_queues[s]
 
 
 class SystemdHandler(logging.Handler):
